a14431117ee40a7395fbc125d2487e3a35091e70
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-03-28 10:32:09 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (64.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
-//tabulation
+
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        
-        dp[0]=dp[1]=1;
-        
+        int prev=1;
+        int curr=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int next=prev+curr;
+            prev=curr;
+            curr=next;
         }
-        return dp[n];
+        return curr;
     }
 }
