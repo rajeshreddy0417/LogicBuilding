6a0e453a682beea499c2c243d4b3af8ee5944b2e
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-03-28 10:22:34 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (37.87%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        HashMap <Integer,Integer> hm=new HashMap<>();
+        
+        return fun(n,hm);
+        
+    }
+    public int fun(int n,HashMap<Integer,Integer> hm){
+        if(n==0 || n==1){
+            return 1;
+        }
+        
+        if(!hm.containsKey(n)){
+            hm.put(n,fun(n-1,hm)+fun(n-2,hm));
+        }
+        return hm.get(n);
+        
+        
+        
+        
+        
+    }
+}
