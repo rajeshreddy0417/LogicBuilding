7106cd2b449293cd77e6a37ab81bf9124d5ff142
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-06-08 10:59:04 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (70.72%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,8 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
-        double a=0.0000;
-        
-        a=a+Math.pow(x,n);
-        return a;
+        double ans=0.0000;
+        ans=Math.pow(x,n);
+        return ans;
     }
 }
