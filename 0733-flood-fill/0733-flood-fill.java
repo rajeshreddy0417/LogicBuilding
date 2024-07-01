7f87352f8958301810ad12a834f46f9da4659bb3
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-07-01 13:19:15 +0530

    Time: 1 ms (42.74%), Space: 44.7 MB (54.83%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,44 +1,31 @@
-//BFS approach
-class Pair{
-    int row;
-    int col;
-    public Pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
+//DFS approach
 
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int a=image[sr][sc];
         int m=image.length;
         int n=image[0].length;
         if(image[sr][sc]==color){
             return image;
         }
         
-        Queue<Pair> que=new LinkedList<Pair>();
-        que.add(new Pair(sr,sc));
-        
-        while(!que.isEmpty()){
-            int r=que.peek().row;
-            int c=que.peek().col;
-            image[r][c]=color;
-            que.remove();
-            
-            int[] rr={-1,0,0,1};
-            int[] cc={0,-1,+1,0};
-            
-            for(int l=0;l<rr.length;l++){
-                int i=r+rr[l];
-                int j=c+cc[l];
-            
-            
-            if(i>=0 && j>=0 && i<m && j<n && image[i][j]==a){
-                que.add(new Pair(i,j));
-            }
-            }
-        }
+        DFS(sr,sc,image,color,image[sr][sc]);
         return image;
     }
+    
+    public static void DFS(int i,int j,int[][] image,int color,int ins){
+        int[] rw={-1,0,0,1};
+        int[] cw={0,-1,1,0};
+        image[i][j]=color;
+        for(int l=0;l<rw.length;l++){
+            int row=i+rw[l];
+            int col=j+cw[l];
+            
+            if(row>=0 && col>=0 && row<image.length && col<image[0].length && image[row][col]==ins){
+                DFS(row,col,image,color,ins);
+            }
+            
+            
+        }
+    }
+    
 }
