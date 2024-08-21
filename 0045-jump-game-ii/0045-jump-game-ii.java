50a454dad9894f2ed698b5af8facfec70e3e73b4
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-08-22 00:36:21 +0530

    Time: 2 ms (59.31%), Space: 44.4 MB (99.35%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int ind=0;
+        int count=0;
+        int end=0;
+        for(int i=0;i<n-1;i++){
+            ind=Math.max(ind,i+nums[i]);
+            if(i==end){
+                count++;
+                end=ind;
+            }
+        }
+        return count;
+    }
+}
