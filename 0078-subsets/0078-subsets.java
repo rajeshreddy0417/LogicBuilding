844f908e2339d03fb2f98f854dac5e72d2933991
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-10-05 11:52:43 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (84.02%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        
-        List<List<Integer>> a=new ArrayList<>();
-        List<Integer> sa=new ArrayList<>();
-        fun(nums,0,sa,a);  
-        System.out.println(a);
-        //System.out.prinytln(Arrays.toString(list.toArray(a)));
-        return a;
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> curr=new ArrayList<>();
+        fun(nums,0,ans,curr);
+        return ans;
     }
-    
-    public void fun(int[] arr,int i,List<Integer> sa,List<List<Integer>> a){
-        if(i==arr.length){
-            a.add(new ArrayList<Integer>(sa));
-            return;
+    public static void fun(int[] nums,int ind,List<List<Integer>> ans,List<Integer> curr){
+        //List<Integer> curr=new ArrayList<>();
+        if(ind==nums.length){
+            ans.add(new ArrayList<>(curr));
+            //curr.clear();
+            return ;
         }
-        
-        fun(arr,i+1,sa,a);
-        sa.add(arr[i]);
-        fun(arr,i+1,sa,a);
-        sa.remove(sa.size()-1);
+        fun(nums,ind+1,ans,curr);
+        curr.add(nums[ind]);
+        fun(nums,ind+1,ans,curr);
+        curr.remove(curr.size()-1);
+        //return;
+        //return ans;
     }
 }
