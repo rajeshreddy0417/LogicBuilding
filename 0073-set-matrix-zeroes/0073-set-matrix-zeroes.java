b8b7721efc97506472738996794a064b3a6ab8d0
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-12-17 10:12:09 +0530

    Time: 1 ms (75.73%), Space: 45.6 MB (43.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,31 +1,29 @@
-class Solution {
-    public void setZeroes(int[][] matrix) {
-        ArrayList<Integer> id=new ArrayList<>();
-        ArrayList<Integer> jd=new ArrayList<>();
-        
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    id.add(i);
-                    jd.add(j);
-                }
-            }
-        }
-        
-        System.out.println(id);
-        System.out.println(jd);
-        
-        
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(id.contains(i) || jd.contains(j)){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-        
-        
+class Pair{
+    int i,j;
+    public Pair(int i,int j){
+        this.i=i;
+        this.j=j;
     }
-    
-    
+}
+
+class Solution {
+    public void setZeroes(int[][] arr) {
+        ArrayList<Pair> al=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]==0){
+                    al.add(new Pair(i,j));
+                }
+            }
+        }
+        
+        for(int i=0;i<al.size();i++){
+            for(int j=0;j<arr.length;j++){
+                arr[j][al.get(i).j]=0;
+            }
+            for(int j=0;j<arr[0].length;j++){
+                arr[al.get(i).i][j]=0;
+            }
+        }
+    }
 }
