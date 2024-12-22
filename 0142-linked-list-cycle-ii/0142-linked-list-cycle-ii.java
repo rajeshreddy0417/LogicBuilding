5d43d07d01772495d41b7cb507c28fb4d54073e4
Rajesh Reddy Pulakunta <136630958+rajeshreddy0417@users.noreply.github.com>
2024-12-22 19:06:36 +0530

    Time: 1 ms (28.32%), Space: 45 MB (6.99%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,65 +9,34 @@
  *     }
  * }
  */
-/*
-finding the node where cycle starts cycle
-
-steps:
-find length of the cycle
-move s ahead by length of cycle times
-move s and f pointers by one ,it will meet at start
-*/
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode node=head;
-        ListNode slow=head;
-        ListNode fast=head;
-        int length=len(head,slow,fast);
-        System.out.println(length);
-        return loc(head,length);
-    }
-    
-    public int len(ListNode node,ListNode slow,ListNode fast){
-        
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast){
-                ListNode temp=slow;
-                int length=0;
-                do{
-                    temp=temp.next;
-                    length=length+1;
-                }while(temp!=slow);
-                return length;
-                
-            }
-            
-        }
-        
-        return 0;
-    }
-    
-    public ListNode loc(ListNode node,int length){
-        if(length==0){
+        if(head==null || head.next==null){
             return null;
         }
-        
-        ListNode fir=node;
-        ListNode sec=node;
-        
-        while(length>0){
-            sec=sec.next;
-            length--;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                break;
+            }
         }
-        
-        while(fir!=sec){
-            fir=fir.next;
-            sec=sec.next;
+        // ListNode curr=head;
+        // int i=0;
+        // while(curr!=slow){
+        //     curr=curr.next;
+        //     i++;
+        // }
+        // return i;
+        if(fast.next==null || fast.next.next==null){
+            return null;
         }
-        return fir;
-        
-        
+        while(head!=slow){
+            head=head.next;
+            slow=slow.next;
+        }
+        return head;
     }
-    
 }
